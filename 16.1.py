b431f8350ef2362be4c5fb7aa6297b2f72cab1be
Serge Kor <shmelya008@gmail.com>
2024-10-12 17:40:22 +0300

    Update 16.1.py

--- 16.1.py
+++ 16.1.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {'message': 'Главная страница'}
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+@app.get("/user/{user_id}")
+async def userid(user_id: int) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+@app.get("/user")
+async def user_info(username: str = 'name', age: int = 0) -> dict:
+    return {'message': f'Информация о пользователе: Имя: {username}, Возраст: {age}'}
+  
